f146e1e1cc01640ad81518b1a5c6ebb456f53cfd
Henry Owens <henryowens@Henrys-MacBook-Pro.local>
2023-05-04 22:43:56 +0200

    Adding chakra

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,10 +1,11 @@
+import { Button, Heading } from "@chakra-ui/react";
 import { PropsWithChildren } from "react";
 
 const Layout: React.FC<PropsWithChildren> = ({ children }) => (
   <div id="Layout">
     <header>
-      <h2>HENRY OWENS</h2>
-      <button>Contact</button>
+      <Heading as="h2">HENRY OWENS</Heading>
+      <Button>Contact</Button>
     </header>
     {children}
   </div>
